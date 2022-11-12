db01a877b2e0321acd046b7fb473e1a1b4705786
AlexandruNuta <georgian.nuta@student.unitbv.ro>
2022-11-12 17:35:06 +0200

    Added an overload of "<<" operator for "Map" class

--- Triviador/Map.cpp
+++ Triviador/Map.cpp
@@ -1,12 +1,27 @@
 #include "Map.h"
 
-Map::Map(const uint8_t& rows,const uint8_t& column)
+Map::Map(const uint8_t& rows,const uint8_t& columns)
 {
 	m_map.resize(rows,std::vector<Region>(columns));
 
 }
-
 std::vector<std::vector<Region>> Map::getMap()
 {
 	return m_map;
+}
+
+std::ostream& operator<<(std::ostream& out,Map map)
+{
+	for (auto row = 0; row <map.m_map.size(); row++)
+	{
+		for (auto column = 0; column < map.m_map[row].size(); column++)
+		{
+			out << "Region: " << row << column << std::endl;
+			out << "Is base: " << map.m_map[row][column].getIsBase() << std::endl;
+			out << "Score: " << map.m_map[row][column].getScore() << std::endl;
+			out << "Owned: " << map.m_map[row][column].getOwned() << std::endl;
+		}
+		out<<std::endl;
+	}
+	return out;
 }
--- Triviador/Map.h
+++ Triviador/Map.h
@@ -7,8 +7,10 @@ class Map
 {
 public:
 	Map(const uint8_t& rows,const uint8_t& columns);
+	Map() = default;
 public:
 	std::vector<std::vector<Region>> getMap();
+	friend std::ostream& operator<<(std::ostream& out,Map map);
 private:
 	std::vector<std::vector<Region>> m_map;
 };
