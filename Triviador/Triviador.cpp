cf60ff379f1fe8b2e65b3b31491ab4ceff57c634
AlexandruNuta <georgian.nuta@student.unitbv.ro>
2022-11-27 17:11:23 +0200

    Added a switch statement for the multiple situations of the game

--- Triviador/Triviador.cpp
+++ Triviador/Triviador.cpp
@@ -3,6 +3,7 @@
 #include <string>
 void main()
 {
+	/*
 	Player p1("Codrut", "ppp");
 	Player p2("Nuta", "ppp");
 	Player p3("George", "ppp");
@@ -20,4 +21,49 @@ void main()
 	Interface i(g);
 	i.stageChooseBase();
 	i.stageChoseRegion();
+	*/
+	std::cout << "INCEPUTUL JOCULUI." << std::endl;
+	std::cout << "Introduceti numarul de jucatori:";
+	uint16_t numarJucatori;
+	std::cin >> numarJucatori;
+	//	std::cout << numarJucatori << std::endl;
+	std::string nume, parola;
+	while(numarJucatori!=5)
+	switch (numarJucatori)
+	{
+	case 0:
+		std::cout << "Numarul minim de jucatori este 2,iar numarul maxim este 4." << std::endl;
+		std::cout << "Va rugam sa introduceti un nou numar de jucatori:"; std::cin >> numarJucatori;
+		std::cout << std::endl;
+		break;
+	case 1:
+		std::cout << "Numarul minim de jucatori este 2,iar numarul maxim este 4." << std::endl;
+		std::cout << "Va rugam sa introduceti un nou numar de jucatori:"; std::cin >> numarJucatori;
+		std::cout<< std::endl;
+		break;
+	case 2:
+	{
+		std::cout << "Succes!" << std::endl;
+		std::cout << "Jucatorul 1 sa introduca numele si parola:" << std::endl;
+		std::cout << "Numele:" << std::endl; std::cin >> nume;
+		std::cout << "Parola:" << std::endl; std::cin >> parola;
+		Player p1(nume, parola);
+		std::cout << "Jucatorul 2 sa introduca numele si parola:" << std::endl;
+		std::cout << "Numele:" << std::endl; std::cin >> nume;
+		std::cout << "Parola:" << std::endl; std::cin >> parola;
+		Player p2(nume, parola);
+		std::vector<Player> players;
+		players.push_back(p1);
+		players.push_back(p2);
+		Map map(3, 3);
+		Game game(map, 5, players);
+		Interface i(game);
+		i.stageChooseBase();
+		i.stageChoseRegion();
+		break;
+	}
+	}
 }
+	//Interface i(game);
+	//i.stageChooseBase();
+	//i.stageChoseRegion();
