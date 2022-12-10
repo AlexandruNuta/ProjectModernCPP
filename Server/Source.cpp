ca41d0ff744d3f5ca8029553c2a4a9dcbf87c02f
Coddd <codrutpavel02@gmail.com>
2022-12-10 11:41:43 +0200

    Added a few server commands for testing

--- Server/Source.cpp
+++ Server/Source.cpp
@@ -9,5 +9,13 @@ namespace sql = sqlite_orm;
 
 int main() 
 {
+	crow::SimpleApp app;
 
+	CROW_ROUTE(app, "/hello")([]()
+		{
+			return "Hello World";
+		}
+	);
+
+	app.port(18080).multithreaded().run();
 }
