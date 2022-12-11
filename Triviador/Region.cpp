3a32de7bb1f645423c4598eeaf04a92c09ef93ed
danielmocanu38 <daniel-m.mocanu@student.unitbv.ro>
2022-12-11 19:16:48 +0200

    Added set & get methods for neighbours in "Region" class

--- Triviador/Region.cpp
+++ Triviador/Region.cpp
@@ -1,8 +1,12 @@
 #include "Region.h"
 Region::Region()
 	:m_isBase(false)
-	,m_score(100)
-	,m_owned(false)
+	, m_score(100)
+	, m_owned(false)
+	, m_North(nullptr)
+	, m_East(nullptr)
+	, m_South(nullptr)
+	, m_West(nullptr)
 {
 }
 
@@ -47,4 +51,44 @@ void Region::IncrementScore()
 void Region::DecrementScore()
 {
 	this->m_score -= 100;
+}
+
+Region* Region::getNorthNeighbour()
+{
+	return m_North;
+}
+
+Region* Region::getEastNeighbour()
+{
+	return m_East;
+}
+
+Region* Region::getSouthNeighbour()
+{
+	return m_South;
+}
+
+Region* Region::getWestNeighbour()
+{
+	return m_West;
+}
+
+void Region::setNorthNeighbour(Region* reg)
+{
+	m_North = reg;
+}
+
+void Region::setEasthNeighbour(Region* reg)
+{
+	m_East = reg;
+}
+
+void Region::setSouthhNeighbour(Region* reg)
+{
+	m_South = reg;
+}
+
+void Region::setWesthNeighbour(Region* reg)
+{
+	m_West = reg;
 }
--- Triviador/Region.h
+++ Triviador/Region.h
@@ -14,12 +14,20 @@ public:
 	void ChangeOwned();
 	void IncrementScore();
 	void DecrementScore();
+	Region* getNorthNeighbour();
+	Region* getEastNeighbour();
+	Region* getSouthNeighbour();
+	Region* getWestNeighbour();
+	void setNorthNeighbour(Region* reg);
+	void setEasthNeighbour(Region* reg);
+	void setSouthhNeighbour(Region* reg);
+	void setWesthNeighbour(Region* reg);
 
 private:
 	bool m_isBase;
 	bool m_owned;
 	uint16_t m_score;
-	Region* m_north;
+	Region* m_North;
 	Region* m_East;
 	Region* m_South;
 	Region* m_West;
