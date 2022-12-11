14a222e84d0159c162246802db378b9229f37750
Patrasc George <georgedanielp14@gmail.com>
2022-12-11 17:26:50 +0200

    I added the verifyAnswer method that checks if the answer given by the user is correct and the setAnswer method.

--- Triviador/numericQuestion.cpp
+++ Triviador/numericQuestion.cpp
@@ -1,1 +1,14 @@
 #include "numericQuestion.h"
+
+int numericQuestion::verifyAnswer(const int& answer)
+{
+	if (m_answer - answer)
+		return false;
+	return true;
+}
+
+void numericQuestion::setAnswer(int const& answer)
+{
+	m_answer = answer;
+}
+
