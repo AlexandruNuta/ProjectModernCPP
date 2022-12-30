4551e9172810b7ae812497bffcd46f449aaa2f3b
Patrasc George <georgedanielp14@gmail.com>
2022-12-30 16:29:16 +0000

    I added new methods in the Player class.

--- Triviador/Player.h
+++ Triviador/Player.h
@@ -14,6 +14,15 @@ public:
 	uint16_t GetIndexVector() const;
 	uint16_t TotalScore() const;
 
+	void AddRegion(std::shared_ptr<Region> region);
+	void RemoveRegion(std::shared_ptr<Region> region);
+	bool VerifiVecinity(const std::pair<uint16_t, uint16_t>& coordinates);
+
+	void Conquering(std::shared_ptr<Player> opponent);
+	void Elimination();
+
+	friend std::ostream& operator<< (std::ostream& out, std::shared_ptr<Player> player);
+
 private:
 	std::string m_username;
 	std::string m_password;
